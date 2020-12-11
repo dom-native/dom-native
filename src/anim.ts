b90fbac7a4faefc489399573fa0bdcc7d51c7f46
Jeremy Chone <jeremy.chone@gmail.com>
2020-12-11 13:24:17 -0800

    . anim - minor fix and code comments

--- src/anim.ts
+++ src/anim.ts
@@ -3,15 +3,16 @@
  * Anim callback function. 
  * @param ntime normalized time (0 to 1) and eased if a ease function was provided.
  * @param raftime the requestAnimationFrame time (relative to raf first start time)
+ * @returns if callback returns false, it will end the callbacks regardless of the remaining time.
  */
 
 type AnimCallback = (ntime: number, raftime: number) => void | boolean;
 
 /**
- * Execute the function fn on each requestAnimationFrame with the normalized time as argument [0..1] based on the duration 
+ * Execute the callback on each requestAnimationFrame with the normalized time as argument [0..1] based on the duration 
  * and eventual easing.
  * 
- * Note 1: This used the requestAnimationFrame timestamp, to allow animations synchronization.
+ * Note 1: This use the requestAnimationFrame timestamp, to allow animations synchronization.
  * 
  * Note 2: dom-native does not provide any easing method. d3-ease are a good source of easings.
  * 
@@ -25,33 +26,45 @@ export function anim(callback: AnimCallback, duration: number, ease?: (normTime:
 	return new Promise((res, rej) => {
 
 		requestAnimationFrame(st => {
+			// on first requestAnimationFrame, capture the reference startTime
 			const startRafTime = st;
 
-			drawNext(st);
+			// start the callbacks
+			callNext(st);
 
-			function drawNext(raftime: number) {
+			function callNext(raftime: number) {
 				let done = false;
+
+				// compute the normTime from the start time and duration
 				const normTime = (raftime - startRafTime) / duration;
 				let ntime = normTime; // computed normalized time
 
-				if (raftime - startRafTime > duration) {
+				// if passed duration, set ntime to 1 and make it last call
+				if (raftime - startRafTime >= duration) {
 					ntime = 1;
 					done = true;
-				} else if (ease) {
-					ntime = (ease) ? ease(normTime) : normTime;
+				}
+				// otherwise, if we have a ease, we update the ntime
+				else if (ease) {
+					ntime = ease(normTime);
 				}
 
 				try {
+					// perform the callback
 					const r = callback(ntime, raftime);
 
+					// if the callback returns false, make it last call
 					if (r === false) {
 						done = true;
 					}
 
+					// if done, we resolve the promise
 					if (done) {
 						res();
-					} else {
-						requestAnimationFrame(drawNext);
+					}
+					// otehrwise, we schedule a callNext for the next request animation frame
+					else {
+						requestAnimationFrame(callNext);
 					}
 
 				} catch (ex) {
