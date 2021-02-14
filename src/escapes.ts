68561383f7a7e29fc753cf0e045556eb12689d34
Jeremy Chone <jeremy.chone@gmail.com>
2021-02-14 09:49:47 -0800

    escape - added doc and shortname xh and xa

--- src/escapes.ts
+++ src/escapes.ts
@@ -1,6 +1,9 @@
 
 
 const _elHtml = document.createElement('div');
+/** Shortname for escapeHtml */
+export const xh = escapeHtml;
+/** Escape a HTML content using native DOM textContent to innerHTML capability */
 export function escapeHtml(txt: string) {
 	_elHtml.textContent = txt;
 	const safeTxt = _elHtml.innerHTML;
@@ -9,9 +12,12 @@ export function escapeHtml(txt: string) {
 }
 
 const _elAttr = document.createElement('e');
+/** Shortname for escapeAttr */
+export const xa = escapeAttr;
+/** Escape a string as a HTML Attribute content using native DOM attribute serialization */
 export function escapeAttr(txt: string) {
 	_elAttr.setAttribute('a', txt);
-	// .outerHTML will generate something like '<e a="..."></e>';
+	// .outerHTML will generate '<e a="..."></e>';
 	const html = _elAttr.outerHTML;
 	const safeTxt = html.substring(6, html.length - 6);
 	_elHtml.setAttribute('a', '');
--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ export { adoptStyleSheets, css } from './css';
 export { all, append, AppendPosition, closest, first, next, prev } from './dom';
 export { elem, frag, html } from './dom-builders';
 export { pull, puller, push, pusher } from './dx';
-export { escapeAttr, escapeHtml } from './escapes';
+export { escapeAttr, escapeHtml, xa, xh } from './escapes';
 export { addOnEvents, bindOnEvent, bindOnEvents, off, on, OnEvent, OnEventListener, OnListenerBySelector, trigger } from './event';
 export { addHubEvents, bindHubEvents, hub, Hub, HubBindings, HubEventInfo, unbindHubEvents } from './hub';
 export { className, style } from './style';
