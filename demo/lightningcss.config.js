2e25cfa63bb9af60296bf4ddb5b7788eb3055426
Jeremy Chone <jeremy.chone@gmail.com>
2026-03-10 13:31:09 -0700

    ^ demo-build - log lightningcss demo css build execution

--- demo/lightningcss.config.js
+++ demo/lightningcss.config.js
@@ -6,6 +6,8 @@ const cssInputPath = new URL("./src/css/main.css", import.meta.url).pathname;
 const cssOutputPath = new URL("./web-content/css/demo-bundle.css", import.meta.url).pathname;
 const cssOutputDir = dirname(cssOutputPath);
 
+console.log("[demo] Building CSS with lightningcss...");
+
 let { code, map } = bundle({
 	filename: cssInputPath,
 	map: true,
@@ -13,4 +15,5 @@ let { code, map } = bundle({
 
 mkdirSync(cssOutputDir, { recursive: true });
 writeFileSync(cssOutputPath, code);
+console.log(`[demo] Generated CSS: ${cssOutputPath}`);
 
