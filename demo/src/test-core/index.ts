2be42fe9547c8842e0356d1379441ccbd99dfe8a
Jeremy Chone <jeremy.chone@gmail.com>
2026-03-11 16:10:15 -0700

    . demo-test-core - remove test-position from test navigation

--- demo/src/test-core/index.ts
+++ demo/src/test-core/index.ts
@@ -1,5 +1,4 @@
 export * from "./spec-test-dom.js";
-export * from "./spec-test-position.js";
 export * from "./spec-test-attr.js";
 export * from "./spec-test-builder.js";
 export * from "./spec-test-base.js";
