ae253540522fca569596f5c5df2b9daae0368598
Jeremy Chone <jeremy.chone@gmail.com>
2026-03-13 20:58:56 -0700

    . ui - update build scripts

---
+++ dom-native-ui/rolldown.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "rolldown";
+
+const input = new URL("./src/index.ts", import.meta.url).pathname;
+
+export default defineConfig({
+	input,
+	platform: "browser",
+	tsconfig: new URL("./tsconfig.json", import.meta.url).pathname,
+	output: {
+		file: new URL("./.dist-lib/dom-native-ui.js", import.meta.url).pathname,
+		format: "iife",
+		name: "bundle",
+		sourcemap: true,
+	},
+});
---
+++ dom-native-ui/rolldown.min.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "rolldown";
+
+const input = new URL("./src/index.ts", import.meta.url).pathname;
+
+export default defineConfig({
+	input,
+	output: {
+		file: new URL("./.dist-lib/dom-native-ui.min.js", import.meta.url).pathname,
+		format: "iife",
+		name: "bundle",
+		sourcemap: true,
+		minify: true,
+	},
+});
